c55ddea561b2654db92854546c69c7b96a48d409
dell <aass47382@gmail.com>
2023-04-18 15:55:55 +0200

    final

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ import 'pages/table_screen.dart';
 import 'pages/humidity_screen.dart';
 import 'pages/infrared_screen.dart';
 import 'package:weather/pages/About_screen.dart';
+//mmmmmmmmm
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
