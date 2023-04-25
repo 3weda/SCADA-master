9489701f27a0a40fbe04ffeb81b3f25bd8d5bf02
dell <aass47382@gmail.com>
2023-04-25 23:22:39 +0200

    final

--- lib/pages/report_screen.dart
+++ lib/pages/report_screen.dart
@@ -1,9 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 
-class Report extends StatelessWidget {
+class Report extends StatefulWidget {
   static const String routeName = "report";
 
   const Report({super.key});
+
+  @override
+  _ReportState createState() => _ReportState();
+}
+
+class _ReportState extends State<Report> {
+  final _formKey = GlobalKey<FormState>();
+  final _value1Controller = TextEditingController();
+  final _value2Controller = TextEditingController();
+
+  Future<void> _generatePdf() async {
+    final pdf = pw.Document();
+    final value1 = double.tryParse(_value1Controller.text) ?? 0.0;
+    final value2 = double.tryParse(_value2Controller.text) ?? 0.0;
+
+    pdf.addPage(pw.Page(
+      build: (pw.Context context) {
+        return pw.Center(
+          child: pw.Text('Value 1: $value1\nValue 2: $value2'),
+        );
+      },
+    ));
+
+    final directory = await getApplicationDocumentsDirectory();
+    final file = File('${directory.path}/report.pdf');
+    await file.writeAsBytes(await pdf.save());
+
+    // Share the PDF file via social networking
+    // ...
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +47,60 @@ class Report extends StatelessWidget {
         elevation: 0,
         centerTitle: true,
         backgroundColor: Colors.transparent,
-        title: Text("report"),
+        title: Text("Report"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextFormField(
+                controller: _value1Controller,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Value 1',
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter a value';
+                  }
+                  if (double.tryParse(value) == null) {
+                    return 'Please enter a valid number';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
+                controller: _value2Controller,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Value 2',
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter a value';
+                  }
+                  if (double.tryParse(value) == null) {
+                    return 'Please enter a valid number';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _generatePdf();
+                  }
+                },
+                child: Text('Generate PDF'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
